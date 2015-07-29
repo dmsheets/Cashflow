f35b167fa24fd183e324874415e9a66e167cdc59
dwi-tanto-p <dwitantop@gmail.com>
2015-07-30 00:01:11 +0700

    okeh

---
+++ CashCard/Controllers/CutOffPerBranchReportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CashCard.Models;
+
+namespace CashCard.Controllers
+{
+    public class CutOffPerBranchReportController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //
+        // GET: /CutOffPerBranchReport/
+        public ActionResult Index()
+        {
+            var dt = db.Branches.Select(oo => new { aaa = oo.Id, bbb = oo.Name}).ToList();
+
+            var x = new SelectList(dt, "aaa", "bbb");
+            ViewBag.Branches = x;
+            return View();
+        }
+	}
+}
