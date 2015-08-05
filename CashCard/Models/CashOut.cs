9f45ce9b61401d3a6eafa4c77dbfa919adfa7c91
dwi-tanto-p <dwitantop@gmail.com>
2015-08-05 20:29:44 +0700

    belom semua

---
+++ CashCard/Models/CashOut.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CashCard.Models
+{
+    public class CashOut : CashCard
+    {
+        private IList<CashOutDetail> _regularDetails; 
+        public CashOut()
+        {
+            _regularDetails = new List<CashOutDetail>();
+
+        }
+        public CostCenter CostCenter { get; set; }
+        public TypeOut TypeOut { get; set; }
+
+        public virtual IList<CashOutDetail> RegularDetails
+        {
+            get { return _regularDetails; }
+            set { _regularDetails = value; }
+        }
+
+        public override void SetTotal()
+        {
+            int total = 0;
+            foreach (var x in RegularDetails)
+            {
+                total += x.SubTotal;
+
+
+            }
+            base.Total = total;
+        }
+    }
+}
---
+++ CashCard/Models/Quiz.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
+
+namespace CashCard.Models
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Info { get; set; }
+        [ForeignKey("QuizGroupId")]
+        public virtual QuizGroup QuizGroup { get; set; }
+        public int? QuizGroupId { get; set; }
+        public CostCenter CostCenter { get; set; }
+        public string Note1Label { get; set; }
+        public string Note2Label { get; set; }
+        
+        
+    }
+}
